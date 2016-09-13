4c5ebbabcf54b1f23459da7ddf85adf5e5de22d8
Christopher Thompson <cthompson@dhcp-153-30.EECS.Berkeley.EDU>
2016-09-12 19:44:29 -0700

    Update add-lore.py to serve legacy lorebot needs

--- add-lore.py
+++ add-lore.py
@@ -18,29 +18,35 @@ class Lore(BaseModel):
     lore = peewee.CharField()
     rating = peewee.FloatField()
 
+    def __str__(self):
+        return "[%s] [%s]\n%s" % (self.time, self.author, self.lore)
+
 
 def main():
     lore_file = 'lore.db'
     db.init(lore_file)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('author')
     parser.add_argument('lore', nargs='+', help="blob of lore to save")
     args = parser.parse_args()
-    print(args.lore)
+    lore = ' '.join(args.lore)
+
+    # add-lore "[mike_bloomfield]: how do i use the lorebot "
 
     t = datetime.datetime.now()
 
     # Try to parse plain loreblob, extracting author from []
-    author = args.author
-    lore = ' '.join(args.lore)
-    print(author, lore)
+    author = lore.split(': ')[0].split('[')[1].split(']')[0]
+    loretxt = ': '.join(lore.split(': ')[1:])
 
     db.begin()
     # Check to see if lore already exists (based on author/lore match)
     matches = Lore.select().where(Lore.author == author and Lore.lore == lore).count()
     if matches == 0:
-        Lore.create(time=t, author=author, lore=lore, rating=0)
+        l = Lore.create(time=t, author=author, lore=loretxt, rating=0)
+        print(l)
+    else:
+        print("Lore already exists...")
     db.commit()
 
 
