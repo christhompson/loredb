01e0be969a7323cd2de1ca32937244a92d045565
Christopher Thompson <chris.j.thompson@gmail.com>
2016-09-19 16:54:54 -0700

    Clean up style/organization
    Miscellaneous Python style and organizational fixes.

--- add-lore.py
+++ add-lore.py
@@ -1,39 +1,26 @@
 #!/usr/bin/env python3
 
+'''add-lore.py [DEPRECATED]
+
+Script to let lorebot interface with loredb.
+New uses should go directly through loredb.py.
+'''
+
 import argparse
 import datetime
-import peewee
-
-db = peewee.SqliteDatabase(None)
-
-
-class BaseModel(peewee.Model):
-    class Meta:
-        database = db
-
-
-class Lore(BaseModel):
-    time = peewee.DateTimeField(null=True, index=True)
-    author = peewee.CharField(null=True, index=True)
-    lore = peewee.CharField()
-    rating = peewee.FloatField()
-
-    def __str__(self):
-        return "[%s] [%s]\n%s" % (self.time, self.author, self.lore)
+from loredb import BaseModel, Lore
 
 
 def main():
     lore_file = 'lore.db'
+    db = BaseModel._meta.database
     db.init(lore_file)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('lore', nargs='+', help="blob of lore to save")
     args = parser.parse_args()
     lore = ' '.join(args.lore)
-
-    # add-lore "[mike_bloomfield]: how do i use the lorebot "
-
-    t = datetime.datetime.now()
+    now = datetime.datetime.now()
 
     # Try to parse plain loreblob, extracting author from []
     author = lore.split(': ')[0].split('[')[1].split(']')[0]
@@ -44,8 +31,7 @@ def main():
     matches = Lore.select().where(
         Lore.author == author and Lore.lore == lore).count()
     if matches == 0:
-        l = Lore.create(time=t, author=author, lore=loretxt, rating=0)
-        print(l)
+        Lore.create(time=now, author=author, lore=loretxt, rating=0)
     else:
         print("Lore already exists...")
     db.commit()
